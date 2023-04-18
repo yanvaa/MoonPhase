ee39973a19bd23cbbae7894087175ada5228eecb
Yanva <54475167+yanvaa@users.noreply.github.com>
2023-04-18 18:48:58 +0400

    config + keybind
    added Key Bind to change hud position
    added config

--- src/main/java/pw/yanva/mooninfo/MoonInfoMod.java
+++ src/main/java/pw/yanva/mooninfo/MoonInfoMod.java
@@ -2,16 +2,28 @@ package pw.yanva.mooninfo;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import pw.yanva.mooninfo.config.ModConfig;
+import pw.yanva.mooninfo.events.KeyInputHandler;
+
+import java.io.IOException;
 
 public class MoonInfoMod implements ModInitializer {
     public static final String MOD_ID = "yanva_moonphase";
+    public static ModConfig config;
     PhaseIcon icon;
 
     @Override
-    public void onInitialize() {
+    public void onInitialize(){
+        config = new ModConfig();
         icon = new PhaseIcon();
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
-            icon.drawPhaseIcon(matrixStack);
+            try {
+                config.load();
+                icon.drawPhaseIcon(matrixStack, config.hudPosition);
+            } catch (IOException ie) {
+                return;
+            }
         });
+        KeyInputHandler.register();
     }
 }
--- src/main/java/pw/yanva/mooninfo/PhaseIcon.java
+++ src/main/java/pw/yanva/mooninfo/PhaseIcon.java
@@ -17,7 +17,10 @@ public class PhaseIcon extends DrawableHelper {
     private static final Formatting[] FULLNESS_COLOR = new Formatting[]
             {Formatting.RED, Formatting.GOLD, Formatting.YELLOW, Formatting.GREEN, Formatting.DARK_GREEN};
 
-    void drawPhaseIcon(MatrixStack matrixStack) {
+    void drawPhaseIcon(MatrixStack matrixStack, int hudPosition) {
+        if (hudPosition == 0) {
+            hudPosition = 4;
+        }
         MinecraftClient mc = MinecraftClient.getInstance();
         int windowWidth = mc.getWindow().getScaledWidth();
         int windowHeight = mc.getWindow().getScaledHeight();
@@ -33,21 +36,58 @@ public class PhaseIcon extends DrawableHelper {
         } else if (mc.world.getRegistryKey() == World.NETHER) {
             RenderSystem.setShaderTexture(0, MOONPHASE_NETHER);
         }
-        DrawableHelper.drawTexture(matrixStack, windowWidth - 60, windowHeight - 26, (phase)*24, 0, 24, 24, 192, 24);
+        switch (hudPosition) {
+            case (1) -> DrawableHelper.drawTexture(matrixStack, 5, 14, (phase) * 24, 0, 24, 24, 192, 24);
+            case (2) -> DrawableHelper.drawTexture(matrixStack, 5, windowHeight - 26, (phase) * 24, 0, 24, 24, 192, 24);
+            case (3) -> DrawableHelper.drawTexture(matrixStack, windowWidth - 60, 54, (phase) * 24, 0, 24, 24, 192, 24);
+            case (4) -> DrawableHelper.drawTexture(matrixStack, windowWidth - 60, windowHeight - 26, (phase) * 24, 0, 24, 24, 192, 24);
+        }
 
         //fullness
         int sizePercent = (int)(mc.world.getMoonSize() * 100f);
-        mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
-                        FULLNESS_COLOR[sizePercent / 25],
-                        sizePercent,
-                        phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
-                windowWidth - 34, windowHeight - 23, 0xffffff);
 
         //time left
         long daySecondsLeft = (24000 - mc.world.getTimeOfDay() % 24000) / 20;
         long dayMinutesLeft = daySecondsLeft / 60;
         boolean atLeast60s = daySecondsLeft >= 60;
-        mc.textRenderer.drawWithShadow(matrixStack, atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
-                windowWidth - 34, windowHeight - 23 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+
+        switch (hudPosition) {
+            case (1) -> {
+                mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
+                                FULLNESS_COLOR[sizePercent / 25],
+                                sizePercent,
+                                phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
+                        31, 17, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrixStack, atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
+                        31, 17 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+            }
+            case (2) -> {
+                mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
+                                FULLNESS_COLOR[sizePercent / 25],
+                                sizePercent,
+                                phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
+                        31, windowHeight - 23, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrixStack, atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
+                        31, windowHeight - 23 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+            }
+            case (3) -> {
+                mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
+                                FULLNESS_COLOR[sizePercent / 25],
+                                sizePercent,
+                                phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
+                        windowWidth - 34, 57, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrixStack, atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
+                        windowWidth - 34, 57 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+            }
+            case (4) -> {
+                mc.textRenderer.drawWithShadow(matrixStack, String.format("%s%d%%%s",
+                                FULLNESS_COLOR[sizePercent / 25],
+                                sizePercent,
+                                phase <= 3 ? Formatting.RED + "↓" : Formatting.DARK_GREEN + "↑"),
+                        windowWidth - 34, windowHeight - 23, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrixStack, atLeast60s ? dayMinutesLeft + "min" : daySecondsLeft + "s",
+                        windowWidth - 34, windowHeight - 23 + mc.textRenderer.fontHeight, Formatting.DARK_AQUA.getColorValue());
+            }
+        }
     }
 }
